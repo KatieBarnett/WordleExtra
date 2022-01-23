a033781be2f8d0aadb641ffb10421ec6f1d40c14
Katie Barnett <katie.barnett@bilue.com.au>
2022-01-23 21:20:24 +1100

    Added word lists & word processing scripts

---
+++ scripts/textProcess.py
@@ -0,0 +1,8 @@
+import re
+for wordLength in range(10):
+	with open('wordfiles/engmix_out_' + str(wordLength+1) + '_letters.txt', 'w') as outputFile:
+		with open('wordfiles/engmix.txt','r', encoding='unicode_escape') as inputFile:
+		    for line in inputFile:
+		    	strippedLine = line.strip()
+		    	if len(strippedLine) == wordLength+1 and re.match('([a-z]+)$', strippedLine):
+		    		outputFile.write(strippedLine + ',')
